b28bf1ba72e80d6cdb0ec44bde1b46e30c8c8afb
srv <mx501@mail.ru>
2025-09-10 10:56:25 +0700

    Удален неиспользуемый импорт метода Get из контроллера заказов для улучшения читаемости кода.

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 
