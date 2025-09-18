0466cf8251e88b6e932350476ed77b0a251a2e73
srv <mx501@mail.ru>
2025-09-18 10:14:16 +0700

    Добавлена новая роль 'PRODUCT' в перечисление USER_ROLE и соответствующий перевод на русский язык в объекте userRole.

--- src/role/enum/role.enum.ts
+++ src/role/enum/role.enum.ts
@@ -1,11 +1,13 @@
 export enum USER_ROLE {
   ADMIN = 'admin',
+  PRODUCT = 'product',
   MANAGER = 'manager',
   USER = 'user',
 }
 
 export const userRole: Record<USER_ROLE, string> = {
   [USER_ROLE.ADMIN]: 'Администратор',
+  [USER_ROLE.PRODUCT]: 'Продукт',
   [USER_ROLE.MANAGER]: 'Менеджер',
   [USER_ROLE.USER]: 'Пользователь',
 };
