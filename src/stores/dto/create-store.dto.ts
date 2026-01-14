52f6f55e56ffe3eeabe0feb445c0b9ac6a5292e7
ryabovva <reggg2004@mail.ru>
2026-01-14 16:39:53 +0700

    Добавлено: 1. Добавил получение магазинов с фильтрацией по региону, городу и магазину

--- src/stores/dto/create-store.dto.ts
+++ src/stores/dto/create-store.dto.ts
@@ -1,1 +1,29 @@
+import { transformNumberArray } from '@/utils/transform-array';
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsArray, IsOptional, IsString, IsNumber } from 'class-validator';
 export class CreateStoreDto {}
+
+export class SearchStoreDto {
+  @ApiProperty({ description: 'Города', example: ['Кемерово', 'Новосибирск'] })
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  city?: string[];
+
+  @ApiProperty({
+    description: 'Регион',
+    example: ['Кемеровская область', 'Новосибирская область'],
+  })
+  @IsArray()
+  @IsString({ each: true })
+  @IsOptional()
+  region?: string[];
+
+  @ApiProperty({ description: 'Магазины', example: [42002, 42014] })
+  @Transform(({ value }) => transformNumberArray(value))
+  @IsArray()
+  @IsNumber({}, { each: true })
+  @IsOptional()
+  idStore?: number[];
+}
--- src/stores/stores.service.ts
+++ src/stores/stores.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseService } from '@/pg-connect/foodcord/orm/grud-postgres.service';
 import { GRUD_OPERATION } from '@/pg-connect/foodcord/orm/enum/metod.enum';
+import { SearchStoreDto } from './dto/create-store.dto';
 
 @Injectable()
 export class StoresService {
@@ -23,5 +24,51 @@ export class StoresService {
       params: [id],
     });
     return result.rows[0];
+  }
+
+  async findStoresPerParams(searchStoreDto: SearchStoreDto) {
+    const { city, region, idStore } = searchStoreDto;
+
+    // Начинаем с базового запроса
+    let query = `
+      SELECT 
+        id_store::int as "idStore"
+      FROM stores 
+      WHERE 1=1
+    `;
+
+    const params: any[] = [];
+    let paramIndex = 1;
+
+    // Фильтр по городам
+    if (city && city.length > 0) {
+      query += ` AND city = ANY($${paramIndex})`;
+      params.push(city);
+      paramIndex++;
+    }
+
+    // Фильтр по регионам
+    if (region && region.length > 0) {
+      query += ` AND region = ANY($${paramIndex})`;
+      params.push(region);
+      paramIndex++;
+    }
+
+    // Фильтр по ID магазинов
+    if (idStore && idStore.length > 0) {
+      query += ` AND id_store = ANY($${paramIndex})`;
+      params.push(idStore);
+      paramIndex++;
+    }
+
+    const result = (await this.databaseService.executeOperation({
+      operation: GRUD_OPERATION.QUERY,
+      query,
+      params,
+    })) as { rows: { idStore: number }[] };
+
+    const ids: number[] = result.rows.map((row) => row.idStore);
+
+    return { idStore: ids };
   }
 }
